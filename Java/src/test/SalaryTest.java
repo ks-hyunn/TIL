18f0601829aba778c0156f55cc7cd4e361d22a75
Hyun <ksh950921@naver.com>
2022-06-30 17:48:11 +0900

    9일차 Java 수업 - SalaryTest 풀이

--- Java/src/test/SalaryTest.java
+++ Java/src/test/SalaryTest.java
@@ -3,9 +3,9 @@ package test;
 class Employee {
 	int id;
 	String name;
-	int salary;
-	int pay;
-	int totalPay;
+	int salary;// 본봉
+	int pay;// 일반수당
+	int totalPay;// 총급여
 
 	Employee(int id, String name, int salary, int pay) {
 		this.id = id;
@@ -14,21 +14,23 @@ class Employee {
 		this.pay = pay;
 	}
 
-	int addPay() {
-		return totalPay = salary + pay;
+	void addPay() {
+		totalPay = salary + pay;
 	}
 }
 
 class Manager extends Employee {
-	int managerPay;
+	int managerPay;// 간부수당
 
 	Manager(int id, String name, int salary, int pay, int managerPay) {
 		super(id, name, salary, pay);
 		this.managerPay = managerPay;
 	}
 
-	int addPay() {
-		return totalPay = salary + pay + managerPay;
+	void addPay() {
+		super.addPay();
+		totalPay += managerPay;
+//		return totalPay = salary + pay + managerPay;
 	}
 }
 
@@ -38,25 +40,29 @@ class Engineer extends Employee {
 
 	Engineer(int id, String name, int salary, int pay, int qualityPay, int engineerPay) {
 		super(id, name, salary, pay);
-		this.qualityPay = qualityPay;
-		this.engineerPay = engineerPay;
+		this.qualityPay = qualityPay;// 자격수당
+		this.engineerPay = engineerPay;// 기술수당
 	}
 
-	int addPay() {
-		return totalPay = salary + pay + qualityPay + engineerPay;
+	void addPay() {
+		super.addPay();
+		totalPay += qualityPay + engineerPay;
+//		return totalPay = salary + pay + qualityPay + engineerPay;
 	}
 }
 
 class Secretary extends Employee {
-	int secretaryPay;
+	int secretaryPay;// 비서수당
 
 	Secretary(int id, String name, int salary, int pay, int secretaryPay) {
 		super(id, name, salary, pay);
 		this.secretaryPay = secretaryPay;
 	}
 
-	int addPay() {
-		return totalPay = salary + pay + secretaryPay;
+	void addPay() {
+		super.addPay();
+		totalPay += secretaryPay;
+//		return totalPay = salary + pay + secretaryPay;
 	}
 }
 
@@ -69,9 +75,15 @@ public class SalaryTest {
 		e[2] = new Engineer(3000, "박기술", 15000, 7500, 5000, 5000);
 		e[3] = new Secretary(4000, "최비서", 15000, 7000, 10000);
 
+		System.out.println(" 사번 :  이름  :  본봉  :  총급여");
 		for (int i = 0; i < e.length; i++) {
 			e[i].addPay();
 			System.out.println(e[i].id + " : " + e[i].name + " : " + e[i].salary + " : " + e[i].totalPay);
+
+//			e 배열에 Secretary 객체가 들어가면 secretaryPay 변수도 출력
+			if (e[i] instanceof Secretary) {
+				System.out.println(((Secretary) e[i]).secretaryPay);
+			}
 		}
 	}
 
