5c272a4c377a64e8c02afe48b22dfa2074ec387d
Hyun <ksh950921@naver.com>
2022-07-06 17:18:20 +0900

    13일차 Java 수업 - SameSentenceTest 풀이

--- Java/src/test/SameSentenceTest.java
+++ Java/src/test/SameSentenceTest.java
@@ -4,20 +4,30 @@ import java.util.ArrayList;
 
 class SameSentence {
 	ArrayList compare(ArrayList list1, ArrayList list2) {
-		ArrayList result = new ArrayList();
-		if (list1.size() == list2.size()) {
-			for (int i = 0; i < list1.size(); i++) {
+		ArrayList result = new ArrayList(4); // 기본값 10
+		int len1 = list1.size();
+		int len2 = list2.size();
+		if (len1 == len2) {
+			for (int i = 0; i < len1; i++) {
 				if (list1.get(i).equals(list2.get(i))) {
-					result.add(i + "번째 인덱스의 문자열 = " + list1.get(i));
+					result.add(list2.get(i));
 				}
 			}
-			return result;
-		} else {
-			result.add("첫번째 배열의 갯수 = " + list1.size());
-			result.add("두번째 배열의 갯수 = " + list2.size());
-			result.add("총 배열의 갯수 = " + (list1.size() + list2.size()));
-			return result;
 		}
+		return result;
+//		if (list1.size() == list2.size()) {
+//			for (int i = 0; i < list1.size(); i++) {
+//				if (list1.get(i).equals(list2.get(i))) {
+//					result.add(i + "번째 인덱스의 문자열 = " + list1.get(i));
+//				}
+//			}
+//			return result;
+//		} else {
+//			result.add("첫번째 배열의 갯수 = " + list1.size());
+//			result.add("두번째 배열의 갯수 = " + list2.size());
+//			result.add("총 배열의 갯수 = " + (list1.size() + list2.size()));
+//			return result;
+//		}
 	}
 }
 
@@ -43,10 +53,26 @@ public class SameSentenceTest {
 
 		SameSentence ss = new SameSentence();
 		ArrayList result1 = ss.compare(list1, list2);
-		System.out.println(result1);
+//		System.out.println(result1);
+		if (result1.size() != 0) {
+			for (int i = 0; i < result1.size(); i++) {
+				System.out.println(result1.get(i));
+			}
+		} else {
+			System.out.println("list1의 갯수 = " + list1.size());
+			System.out.println("list2의 갯수 = " + list2.size());
+		}
 
 		ArrayList result2 = ss.compare(list1, list3);
-		System.out.println(result2);
+//		System.out.println(result2);
+		if (result2.size() != 0) {
+			for (int i = 0; i < result2.size(); i++) {
+				System.out.println(result2.get(i));
+			}
+		} else {
+			System.out.println("list1의 갯수 = " + list1.size());
+			System.out.println("list3의 갯수 = " + list3.size());
+		}
 
 	}
 
