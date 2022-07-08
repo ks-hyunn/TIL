61d0ab0c971e3833ee9c2d59d84fe27055ba2dd5
Hyun <ksh950921@naver.com>
2022-07-08 17:13:12 +0900

    15일차 Java 수업 - Sacnner,FileWriter

--- Java/src/chap18/FileCopyTest2.java
+++ Java/src/chap18/FileCopyTest2.java
@@ -3,29 +3,26 @@ package chap18;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class FileCopyTest {
-//	2byte씩
+public class FileCopyTest2 {
+
 	public static void main(String[] args) {
 		FileReader fr = null;
 		FileWriter fw = null;
 		String inputFile = "src/chap18/FileClassTest.java";
-		String outputFile = "src/chap18/b.txt";
+		String outputFile = "src/chap18/c.txt";
 
 		try {
 			fr = new FileReader(inputFile);
 			fw = new FileWriter(outputFile, true);
 			int i = 1;
-			fw.write(i++ + "라인 : ");
-			while (true) {
-				int result = fr.read();
-				if (result == -1) {
-					break;
-				} else if (result == '\n') {
-					fw.write(i++ + "라인 : ");
-				} else {
-					fw.write(result);
-				}
+
+			Scanner sc = new Scanner(fr);
+
+			while (sc.hasNextLine()) {
+				String fileLine = sc.nextLine() + "\n";
+				fw.write(i++ + "라인 : " + fileLine);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
