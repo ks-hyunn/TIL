45741f32cd67783e9bab895083f592a561b1085f
Hyun <ksh950921@naver.com>
2022-07-15 14:23:00 +0900

    20일차 MySQL 수업 - IN, NOT IN

--- MySQL/day4_UNION, UNION ALL, NOT IN.sql
+++ MySQL/day4_UNION, UNION ALL, NOT IN.sql
@@ -12,3 +12,34 @@ select employee_id, first_name, job_id, department_id from emp_job_man; -- 12개
 select employee_id, first_name, job_id, department_id from emp_dept_50 union select employee_id, first_name, job_id, department_id from emp_job_man; -- 52개(중복 빼고)
 -- union all - 중복 허용
 select employee_id, first_name, job_id, department_id from emp_dept_50 union all select employee_id, first_name, job_id, department_id from emp_job_man; -- 57개(중복 포함)
+-- not in, in
+-- select 안에 select(subquery)
+-- employees 테이블에서 이름이 kelly와 같은 부서에서 근무하는 사원의 이름 근무부서코드 조회
+select department_id from employees where first_name = 'kelly'; -- kelly의 부서번호 조회
+select first_name, department_id from employees where department_id = 50; -- kelly와 같은 부서에서 근무하는 사원 조회
+select first_name, department_id from employees where department_id = (select department_id from employees where first_name = 'kelly'); -- 첫번째 select를 50에 입력
+select first_name, department_id from employees where department_id = (select first_name, department_id from employees where first_name = 'kelly'); -- error - 메인쿼리 department_id로 서브쿼리의 first_name과 department_id값 중 어느것 사용? 갯수 종류 동일해야 함
+-- employees 테이블에서 이름이 kelly와 같은 부서이면서 같은 급여받는 사원의 이름 근무부서코드 조회
+select first_name, department_id from employees where department_id = (select department_id from employees where first_name = 'kelly') and salary = (select salary from employees where first_name = 'kelly');
+select first_name, department_id from employees where (salary, department_id) = (select salary, department_id from employees where first_name = 'kelly');
+
+select first_name, department_id, job_id from employees where (job_id, department_id) = (select job_id, department_id from employees where first_name = 'kelly');
+select first_name, department_id, job_id from employees where (job_id, department_id) = (select job_id, department_id from employees where first_name = 'kelly') and first_name != 'kelly';
+-- in - 서브쿼리 사용해서 where 절 조건을 만들때 결과가 단일행(1개) 이면 '=' / 다중행(2개이상) 이면 'in'
+select first_name, department_id from employees where department_id = (select department_id from employees where first_name = 'peter'); -- error
+select job_id, department_id from employees where first_name = 'peter'; -- 피터 3명(50번, 80번)
+select first_name, department_id from employees where department_id in (select department_id from employees where first_name = 'peter');
+-- 집계함수는 결과 1개
+select first_name, department_id, salary from employees where salary = (select truncate(avg(salary), -2) from employees);
+-- not in
+select first_name, department_id from employees where department_id not in (select department_id from employees where first_name = 'peter'); -- 50번, 80번 제외한 부서 근무 사원
+
+
+
+
+
+
+
+
+
+
